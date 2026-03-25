5330712390088fa8785a35a1f2b5abe99640a23b
Ercan SORMAZ <ercansormaz@gmail.com>
2026-03-25 16:03:28 +0300

    RC version support

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 docker buildx build --platform linux/amd64,linux/arm64 -f 7.2/Dockerfile --push -t ercansormaz/valkey-cluster:7.2.12 -t ercansormaz/valkey-cluster:7.2 .
 docker buildx build --platform linux/amd64,linux/arm64 -f 8.0/Dockerfile --push -t ercansormaz/valkey-cluster:8.0.7 -t ercansormaz/valkey-cluster:8.0 .
 docker buildx build --platform linux/amd64,linux/arm64 -f 8.1/Dockerfile --push -t ercansormaz/valkey-cluster:8.1.6 -t ercansormaz/valkey-cluster:8.1 .
-docker buildx build --platform linux/amd64,linux/arm64 --push -t ercansormaz/valkey-cluster:9.0.3 -t ercansormaz/valkey-cluster:9.0 -t ercansormaz/valkey-cluster:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -f 9.0/Dockerfile --push -t ercansormaz/valkey-cluster:9.0.3 -t ercansormaz/valkey-cluster:9.0 -t ercansormaz/valkey-cluster:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -f 9.1-rc1/Dockerfile --push -t ercansormaz/valkey-cluster:9.1.0-rc1 -t ercansormaz/valkey-cluster:9.1-rc1 .
