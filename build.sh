8e1833a4db2faa7520354e43607dbfcb62ef499b
Ercan SORMAZ <ercansormaz@gmail.com>
2026-07-22 11:39:25 +0300

    Latest versions to 7.2.14, 8.0.10, 8.1.9, 9.0.5, 9.1.1

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd64,linux/arm64 -f 7.2/Dockerfile --push -t ercansormaz/valkey-cluster:7.2 -t ercansormaz/valkey-cluster:7.2.13 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 8.0/Dockerfile --push -t ercansormaz/valkey-cluster:8.0 -t ercansormaz/valkey-cluster:8.0.9 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 8.1/Dockerfile --push -t ercansormaz/valkey-cluster:8.1 -t ercansormaz/valkey-cluster:8.1.8 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 9.0/Dockerfile --push -t ercansormaz/valkey-cluster:9.0 -t ercansormaz/valkey-cluster:9.0.4 .
-docker buildx build --platform linux/amd64,linux/arm64 -f 9.1/Dockerfile --push -t ercansormaz/valkey-cluster:9.1 -t ercansormaz/valkey-cluster:9.1.0 -t ercansormaz/valkey-cluster:latest .
+docker buildx build --platform linux/amd64,linux/arm64 -f 7.2/Dockerfile --push -t ercansormaz/valkey-cluster:7.2 -t ercansormaz/valkey-cluster:7.2.14 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 8.0/Dockerfile --push -t ercansormaz/valkey-cluster:8.0 -t ercansormaz/valkey-cluster:8.0.10 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 8.1/Dockerfile --push -t ercansormaz/valkey-cluster:8.1 -t ercansormaz/valkey-cluster:8.1.9 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 9.0/Dockerfile --push -t ercansormaz/valkey-cluster:9.0 -t ercansormaz/valkey-cluster:9.0.5 .
+docker buildx build --platform linux/amd64,linux/arm64 -f 9.1/Dockerfile --push -t ercansormaz/valkey-cluster:9.1 -t ercansormaz/valkey-cluster:9.1.1 -t ercansormaz/valkey-cluster:latest .
 docker buildx build --platform linux/amd64,linux/arm64 -f unstable/Dockerfile --push -t ercansormaz/valkey-cluster:unstable .
