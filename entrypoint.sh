fae2fb95ba306eb560afef3a1f2291f6b5d4a9eb
Ercan SORMAZ <ercansormaz@gmail.com>
2026-09-19 17:32:47 +0300

    Automatic cluster initialization support add

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,17 @@ echo "Starting Valkey Cluster..."
 
 /usr/local/bin/create-cluster start
 
+if [ ! -f /data/.cluster_created ]; then
+    echo "First boot detected. Waiting for Valkey nodes to respond..."
+    
+    until redis-cli -p 30001 ping > /dev/null 2>&1; do
+        sleep 1
+    done
+
+    echo "Initializing Valkey Cluster automatically..."
+    echo "yes" | /usr/local/bin/create-cluster create
+    touch /data/.cluster_created
+    echo "Valkey Cluster created successfully!"
+fi
+
 tail -f /dev/null
